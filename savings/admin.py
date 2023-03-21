da167dd7db3875a0babb7ae20c23d9e68f395d79
Madele <mtheron@ssi.engineering>
2023-03-21 18:32:33 +0200

    update settings & requirements w/ djmoney

--- savings/admin.py
+++ savings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from savings.models import *
 
 # Register your models here.
+
+admin.site.register(Balance)
+admin.site.register(Deposit)
+admin.site.register(SavingGoal)
