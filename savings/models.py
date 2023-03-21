ac78e22ea4144559988243ea933b30b59b9805c1
Madele <mtheron@ssi.engineering>
2023-03-21 22:08:14 +0200

    changed SavingGoal to Goal & added logic to models

--- savings/models.py
+++ savings/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from djmoney.models.fields import MoneyField, CurrencyField
 import datetime
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Balance(models.Model):
@@ -13,7 +15,7 @@ class Balance(models.Model):
         return f"{self.name} - {self.amount}"
 
 
-class SavingGoal(models.Model):
+class Goal(models.Model):
     STATUS_CHOICES = (
         ("locked", "Locked"),
         ("unlocked", "Unlocked"),
@@ -22,16 +24,28 @@ class SavingGoal(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     date_added = models.DateField(default=datetime.date.today)
-    image = models.ImageField(upload_to="media/goal/", blank=True, null=True)
-    url_link = models.URLField(blank=True)
     cost = MoneyField(
         max_digits=10, decimal_places=2, default_currency="ZAR", default=0
     )
-    date_completed = models.DateField(blank=True, null=True)
+    image = models.ImageField(upload_to="media/goal/", blank=True, null=True)
+    url_link = models.URLField(blank=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="locked")
+    total_saved = models.ForeignKey(Balance, on_delete=models.CASCADE)
+    date_completed = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.name} - {self.cost} - {self.status}"
+
+    # unlocked
+    def is_unlocked(self):
+        return self.total_saved.amount >= self.cost
+
+    def save(self, *args, **kwargs):
+        if self.status == "completed":
+            post_save.connect(credit_balance, sender=Goal)
+        elif self.is_unlocked():
+            self.status = "unlocked"
+        super().save(*args, **kwargs)
 
 
 class Deposit(models.Model):
@@ -39,6 +53,25 @@ class Deposit(models.Model):
         max_digits=10, decimal_places=2, default_currency="ZAR", default=0
     )
     date_added = models.DateField(default=datetime.date.today)
+    total_saved = models.ForeignKey(Balance, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Amount added: {self.amount}"
+        return f"Amount added: {self.amount}. Balance: {self.total_saved.amount}"
+
+
+# Signals / Triggers / Logic
+
+
+# Add deposit to balance
+@receiver(post_save, sender=Deposit)
+def debit_balance(sender, instance, **kwargs):
+    balance = instance.total_saved
+    balance.amount += instance.amount
+    balance.save()
+
+
+# Credit balance when goal is completed
+def credit_balance(sender, instance, **kwargs):
+    balance = instance.total_saved
+    balance.amount -= instance.cost
+    balance.save()
