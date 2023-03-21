93ce0b776927559335d88dae7c93c987cd81bfab
Madele <mtheron@ssi.engineering>
2023-03-21 17:35:32 +0200

    gitignore & db & added apps to settings

--- savingbuckets/settings.py
+++ savingbuckets/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "savingbuckets",
+    "savings",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
