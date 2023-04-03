7ac2b0467dd418afa61cf9127e99bf4816baa590
Madele <madele.theron@gmail.com>
2023-04-03 20:29:39 +0200

    playing around with the main page text and styles

--- savings/views.py
+++ savings/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from .models import Goal, Balance, Deposit
 
 # Create your views here.
-def home(request): # this can be the landing page :)
-    return render(request, "home.html")
+def home(request): # this can be the landing page :) // or just the main over view page??
+    goals = Goal.objects.all()
+    accounts = Balance.objects.all()
+    return render(request, "home.html", {"goals": goals, "accounts":accounts})
 
 def overview(request):
     return render(request, "overview.html")
 
 def history(request):
-    return render(request, "history.html")
+    completed_goals = Goal.objects.filter(status="completed")
+    return render(request, "history.html", {"completeedgoals": completed_goals})
 
 def contact(request):
     return render(request, "contact.html")
 
-def savings_goal(request):
-    return render(request, "savings_goal.html")
+# this will be the page to view and edit an individual goal
+def savings_goal(request, pk):
+    goal = Goal.objects.get(pk=pk)
+    return render(request, "savings_goal.html", {"goal": goal})
+
